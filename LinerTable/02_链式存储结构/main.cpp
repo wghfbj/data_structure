9c632c0aa5f41680e235bbaf28f774ce99452fa4
fanbaoju <fanbaoju@cvte.com>
2018-11-28 21:30:15 +0800

    [feature][CVTE][LinerTable]Add SeqList & LinkList source file
    [what]添加线性表中顺序和链式存储结构资源问题.
    [why]new feature.
    [how]add it使用Dev C++编译通过.

---
+++ LinerTable/02_链式存储结构/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Linklist.h"
+
+#include "Linklist.cpp"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+	Linklist<int> *L = new Linklist<int>();
+	int a = 0;
+	int b = 1;
+	int c = 2;
+	int d = 3;
+	int e = 4;
+	L->AddLinklist(&a, 0);
+	L->AddLinklist(&b, 0);
+	L->AddLinklist(&c, 0);
+	L->AddLinklist(&d, 0);
+	L->AddLinklist(&e, 0);
+	printf("\n 11 Get Linklist length = %d \n", (int)L->GetLinklistLength());
+	L->ShowLinklist();
+	printf("\n Get Index[%d] value = %d \n", 4, (int)L->GetLinklist(4));
+	printf("\n Delete Index[%d] value = %d \n", 3, (int)L->DeleteLinklist(3));
+	printf("\n 22 Get Linklist length = %d \n", (int)L->GetLinklistLength());
+	L->ShowLinklist();
+	printf("\n Func End ! \n\n");
+	return 0;
+}
