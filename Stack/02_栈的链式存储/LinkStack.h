2e5a7689cf25dec9f9dd72f869e5be701df6134b
fanbaoju <137797309@qq.com>
2018-12-05 21:57:21 +0800

    [feature][CVTE][LinkStack]Add LinkStack source file
    [what]添加栈的链式存储功能.
    [why]需求.
    [how]部分完善.

--- Stack/02_栈的链式存储/LinkStack.h
+++ Stack/02_栈的链式存储/LinkStack.h
@@ -3,6 +3,24 @@
 
 #include "Linklist.h"
 
+template <class TLinklist>
+class LinkStack
+{
+public:
+	LinkStack(void);
+	~LinkStack(void);
+	
+	bool Clear_LinkStack(void);
+	bool Push_LinkStack(TLinklist *data);
+	TLinklist Pop_LinkStack(void);
+	TLinklist GetTop_LinkStack(void);
+	int GetLength_LinkStack(void);
+	int GetMaxLength_LinkStack(void);
+
+private:
+	Linklist<TLinklist> *S;
+};
+
 
 
 #endif //_LINKSTACK_H_
