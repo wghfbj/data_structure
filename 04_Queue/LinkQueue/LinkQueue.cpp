61849d80258ec659fc6d53ce9fc7e269ff2e671d
fanbaoju <137797309@qq.com>
2018-12-15 17:22:17 +0800

    [feature][CVTE][LinkQueue]Feature LinkQueue
    [what]完善LinkQueue功能.
    [why]需求.
    [how]Feature it.

---
+++ 04_Queue/LinkQueue/LinkQueue.cpp
@@ -0,0 +1,46 @@
+#include "LinkQueue.h"
+#include "Linklist.cpp"
+
+
+LinkQueue::LinkQueue(void)   //O(1)
+{
+	L = new Linklist<int>();
+}
+
+
+LinkQueue::~LinkQueue(void)   //O(1)
+{
+	if(L != NULL)
+	{
+		delete(L);
+		L = NULL;
+	}
+}
+
+
+bool LinkQueue::LinkQueue_Clear(void)   //O(n)
+{
+	return L->ClearLinklist();
+}
+
+int LinkQueue::LinkQueue_Append(int *data)   //O(1)
+{
+	return L->AddLinklist(data, L->GetLinklistLength());
+}
+
+int LinkQueue::LinkQueue_Retrieve(void)   //O(1)
+{
+	return L->DeleteLinklist(0);
+}
+
+int LinkQueue::LinkQueue_Header(void)   //O(1)
+{
+	return L->GetLinklist(0);
+}
+
+int LinkQueue::LinkQueue_GetLength(void)   //O(1)
+{
+	return L->GetLinklistLength();
+}
+
+
